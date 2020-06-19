aba66c5b3152801460af3de81a1decd0e304e966
jsmithxyz <jordansmith3813@gmail.com>
2020-06-19 15:12:52 -0400

    added animation to ul items

--- src/LandingPage/Landing.js
+++ src/LandingPage/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "animate.css";
 import "./Landing.css";
 
 class Landing extends React.Component {
@@ -6,13 +7,44 @@ class Landing extends React.Component {
     return (
       <div className='landing-container'>
         <div className='landing-content'>
-          <header className='landing-header'>Dawitt/Maldonado Studios</header>
+          <header className='landing-header animate__animated animate__slideInDown'>
+            Dawitt/Maldonado Studios
+          </header>
+          <hr className='landing-line-break'></hr>
           <div className='landing-nav'>
             <ul className='nav-list'>
-              <li className='nav-item'>About</li>
-              <li className='nav-item'>Work</li>
-              <li className='nav-item'>Press</li>
-              <li className='nav-item'>Contact</li>
+              <li className='nav-item'>
+                <a
+                  href='#'
+                  className='nav-link animate__animated animate__fadeIn animate__delay-1s'
+                >
+                  About
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  href='#'
+                  className='nav-link animate__animated animate__fadeIn animate__delay-2s'
+                >
+                  Work
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  href='#'
+                  className='nav-link animate__animated animate__fadeIn animate__delay-3s'
+                >
+                  Press
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  href='#'
+                  className='nav-link animate__animated animate__fadeIn animate__delay-4s'
+                >
+                  Contact
+                </a>
+              </li>
             </ul>
           </div>
         </div>
